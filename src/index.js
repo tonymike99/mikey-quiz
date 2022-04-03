db8471af7e6862a214080388062500016671a8cd
Tony <tonymike99@gmail.com>
2022-04-03 23:24:14 +0530

    added QuizProvider to App

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 import { makeServer } from "./server";
 import { BrowserRouter } from "react-router-dom";
-import { ThemeProvider } from "./hooks/context/index";
+import { ThemeProvider, QuizProvider } from "./hooks/context/index";
 
 // Call make Server
 makeServer();
@@ -13,7 +13,9 @@ ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <ThemeProvider>
-        <App />
+        <QuizProvider>
+          <App />
+        </QuizProvider>
       </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>,
