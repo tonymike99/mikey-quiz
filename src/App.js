a0730656660df8e517203d97d39cbc21d27ebd49
Tony <tonymike99@gmail.com>
2022-04-07 20:22:48 +0530

    renamed categories route

--- src/App.js
+++ src/App.js
@@ -1,34 +1,34 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import {
-    Login,
-    Signup,
-    Home,
-    Categories,
-    Quiz,
-    Result,
-    PageNotFound,
+  Login,
+  Signup,
+  Home,
+  Categories,
+  Quiz,
+  Result,
+  PageNotFound,
 } from "./pages/index";
 import { Header, Footer } from "./components/index";
 
 function App() {
-    return (
-        <>
-            <Header />
+  return (
+    <>
+      <Header />
 
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/signup" element={<Signup />} />
-                <Route path="/categories" element={<Categories />} />
-                <Route path="/quiz/:id" element={<Quiz />} />
-                <Route path="/result" element={<Result />} />
-                <Route path="/*" element={<PageNotFound />} />
-            </Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/categories/:name" element={<Categories />} />
+        <Route path="/quiz/:id" element={<Quiz />} />
+        <Route path="/result" element={<Result />} />
+        <Route path="/*" element={<PageNotFound />} />
+      </Routes>
 
-            <Footer />
-        </>
-    );
+      <Footer />
+    </>
+  );
 }
 
 export default App;
