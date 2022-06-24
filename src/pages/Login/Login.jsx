05706c05acd4ed3502c084b1e5a37497079e785d
Tony <tonymike99@gmail.com>
2022-06-25 00:59:12 +0530

    modified login page

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,6 +1,8 @@
 import styles from "./Login.module.css";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 import { useDocumentTitle } from "../../hooks/custom/index";
+import { useAuth } from "../../hooks/context/index";
 
 function Login() {
   // SET DOCUMENT TITLE
@@ -8,17 +10,97 @@ function Login() {
 
   // ****************************************************************************************************
 
+  const { authenticateLoginDetails } = useAuth();
+  const [passwordType, setPasswordType] = useState("password");
+  const [loginDetails, setLoginDetails] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleEmailOnChange = (e) => {
+    setLoginDetails({ ...loginDetails, email: e.target.value });
+  };
+
+  const handlePasswordOnChange = (e) => {
+    setLoginDetails({ ...loginDetails, password: e.target.value });
+  };
+
+  const handlePasswordType = () => {
+    passwordType === "text"
+      ? setPasswordType("password")
+      : setPasswordType("text");
+  };
+
+  const handleGuestLoginOnClick = () => {
+    setLoginDetails({
+      email: "guest_user@gmail.com",
+      password: "12345",
+    });
+  };
+
+  const handleLoginOnClick = (e) => {
+    e.preventDefault();
+    authenticateLoginDetails(loginDetails);
+  };
+
+  // ****************************************************************************************************
+
   return (
     <div className={styles["main-container"]}>
-      <main className={styles.main}>
-        <section className="login-signup-section">
-          <h2 className="font-montserrat margin-bottom-2">LOGIN</h2>
-          <form className="form-spacing" action="#">
-            <input type="email" id="emailInput" placeholder="Email Address" />
-            <input type="password" id="passwordInput" placeholder="Password" />
-            <button className="btn btn-primary btn-width-100" type="submit">
-              Login
-            </button>
+      <main className="main">
+        <section className="auth">
+          <h3 className="h3">LOGIN</h3>
+
+          <form className="form" action="#">
+            <div className="form-control">
+              <input
+                type="email"
+                id="emailInput"
+                placeholder="Email Address"
+                required
+                value={loginDetails.email}
+                onChange={(e) => handleEmailOnChange(e)}
+              />
+            </div>
+
+            <div className="form-control relative">
+              <input
+                type={passwordType}
+                id="passwordInput"
+                placeholder="Password"
+                required
+                value={loginDetails.password}
+                onChange={(e) => handlePasswordOnChange(e)}
+              />
+              <span
+                className="absolute badge-center-right color-black"
+                onClick={handlePasswordType}
+              >
+                {passwordType === "password" ? (
+                  <i className="fa-solid fa-eye"></i>
+                ) : (
+                  <i className="fa-solid fa-eye-slash"></i>
+                )}
+              </span>
+            </div>
+
+            <div className="form-control">
+              <button
+                className="btn btn-primary btn-width-100 pointer"
+                type="submit"
+                onClick={(e) => handleLoginOnClick(e)}
+              >
+                Login
+              </button>
+            </div>
+
+            <small
+              className="styled-link pointer"
+              onClick={handleGuestLoginOnClick}
+            >
+              Guest Login
+            </small>
+
             <small>
               Don't have an account?
               <Link to="/signup" className="styled-link">
