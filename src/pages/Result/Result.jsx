aeef10c49264b2dd7e62721d7eb8f6d6af5394e0
Tony <tonymike99@gmail.com>
2022-06-25 18:40:22 +0530

    modified result page styles

--- src/pages/Result/Result.jsx
+++ src/pages/Result/Result.jsx
@@ -12,15 +12,25 @@ function Result() {
   const { questions, expectedAnswers, actualAnswers } = useQuiz();
 
   let marks = [];
+  let totalQuestionsCount = Object.keys(questions).length;
+  let correctAnswersCount = Object.keys(questions).reduce(
+    (acc, cur) => (marks[cur] === 5 ? (acc += 1) : acc),
+    0
+  );
+  let incorrectAnswersCount = totalQuestionsCount - correctAnswersCount;
+  let maximumMarks = totalQuestionsCount * 5;
+  let obtainedMarks = correctAnswersCount * 5;
+  let percentageOfCorrectAnswers =
+    (correctAnswersCount / totalQuestionsCount) * 10;
 
   // ****************************************************************************************************
 
   return (
-    <div className={styles["main-container"]}>
-      <main className={styles.main}>
-        <h1 className="text-center margin-2">Result Summary</h1>
+    <main>
+      <section>
+        <h2 className="h2 text-center margin-bottom-2">Result Summary</h2>
 
-        <table class="styled-table">
+        <table className="styled-table">
           <thead>
             <tr>
               <th>Question</th>
@@ -39,35 +49,42 @@ function Result() {
                 <td>
                   {expectedAnswers[question] === actualAnswers[question]
                     ? (marks[question] = 5)
-                    : (marks[question] = -1)}
+                    : (marks[question] = 0)}
                 </td>
               </tr>
             ))}
           </tbody>
         </table>
+      </section>
 
-        <div className="quiz-stats">
-          <h1>More Statistics</h1>
-          <br />
+      <section className="quiz-stats">
+        <p>Total number of questions answered: {totalQuestionsCount}</p>
+        <p>Number of questions answered correctly: {correctAnswersCount}</p>
+        <p>Number of questions answered incorrectly: {incorrectAnswersCount}</p>
+        <p>
+          Percentage of questions answered correctly:{" "}
+          {percentageOfCorrectAnswers} %
+        </p>
+        <p>Maximum marks: {maximumMarks}</p>
+        <p>Obtained marks: {obtainedMarks}</p>
 
-          <p>Total number of questions: {Object.keys(questions).length}</p>
-          <p>
-            Number of correct answers:{" "}
-            {Object.keys(questions).reduce(
-              (acc, cur) => (marks[cur] === 5 ? (acc += 1) : acc),
-              0
-            )}
-          </p>
-          <p>
-            Total marks:{" "}
-            {Object.keys(questions).reduce(
-              (acc, cur) => (acc += marks[cur]),
-              0
-            )}
-          </p>
-        </div>
-      </main>
-    </div>
+        <h3 className="h3">
+          Verdict:{" "}
+          {percentageOfCorrectAnswers === 100
+            ? "Distinction"
+            : percentageOfCorrectAnswers >= 90 &&
+              percentageOfCorrectAnswers < 100
+            ? "Excellent"
+            : percentageOfCorrectAnswers >= 75 &&
+              percentageOfCorrectAnswers < 90
+            ? "Very Good"
+            : percentageOfCorrectAnswers >= 50 &&
+              percentageOfCorrectAnswers < 75
+            ? "Satisfactory"
+            : "Needs improvement"}
+        </h3>
+      </section>
+    </main>
   );
 }
 
