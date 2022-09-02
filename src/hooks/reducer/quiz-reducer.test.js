9e05013c89bfcbb8f8f047e9fe5c924657e27bc8
Tony <tonymike99@gmail.com>
2022-09-02 18:38:47 +0530

    added test cases to mock network calls

--- src/hooks/reducer/quiz-reducer.test.js
+++ src/hooks/reducer/quiz-reducer.test.js
@@ -1,16 +1,52 @@
 import { quizReducer } from "./quiz-reducer";
+const axios = require("axios");
 
-// type Action = {
-//   type: string;
-//   payload: {
-//     questionNumber: string;
-//     question: string;
-//     expectedAnswer: string;
-//     actualAnswer: string;
-//   };
-// };
+jest.mock("axios");
 
 describe("QUIZ REDUCER", () => {
+  beforeAll(() => console.log("connection setup"));
+  afterAll(() => console.log("connection teardown"));
+
+  test("verify successful api response", async () => {
+    const params = {
+      method: "get",
+      baseURL: "https://opentdb.com",
+      url: "/api.php?amount=10&category=10&type=multiple&encode=url3986",
+    };
+
+    const mockedQuizResponse = {
+      id: 1,
+      question: "What is your name?",
+      correct_answer: ["A", "B", "C"],
+      incorrect_answers: "D",
+    };
+
+    axios.request.mockResolvedValue({
+      data: {
+        results: [mockedQuizResponse],
+      },
+    });
+
+    const actualResponse = await axios.request(params);
+
+    expect(axios.request).toBeCalledWith(params);
+    expect(actualResponse.data.results[0]).toEqual(mockedQuizResponse);
+  });
+
+  test("verify failed api response", async () => {
+    const mockedQuizError = { message: "Network Error" };
+
+    axios.request.mockResolvedValue(mockedQuizError);
+
+    const actualResponse = await axios.request({
+      method: "get",
+      baseURL: "https://opentdb.com",
+      url: "/api.php?amount=10&category=10&type=multiple&encode=url3986",
+    });
+
+    expect(actualResponse.message).toEqual(mockedQuizError.message);
+  });
+
   test("add question to questions", () => {
     // ARRANGE
     const initialState = {
